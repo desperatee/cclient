427348c496b1bb2b45f81998a49bedc5a6f85d6d
desperatee <kurczak19038@gmail.com>
2022-07-27 01:19:57 +0200

    Update roundtripper.go

--- roundtripper.go
+++ roundtripper.go
@@ -13,7 +13,7 @@ import (
 	"github.com/Carcraftz/fhttp/http2"
 	"golang.org/x/net/proxy"
 
-	utls "github.com/Carcraftz/utls"
+	utls "github.com/desperatee/utls"
 )
 
 var errProtocolNegotiated = errors.New("protocol negotiated")
